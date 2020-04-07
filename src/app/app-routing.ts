959250a03512223b009174b127313828ed3c1357
arsalan.gohar@avest-group.com <arsalan.gohar@avest-group.com>
2020-04-07 21:31:53 +0500

    static pages

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -4,6 +4,11 @@ import { VideosComponent } from './components/videos/videos.component';
 import { ErrorComponent } from './core/error/error.component';
 import { NoAccessComponent } from './core/error/no-access.component';
 import { PageNotFoundComponent } from './core/error/page-not-found.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { ServiceProviderComponent } from './components/pages/service-provider/service-provider.component';
+import { CharityComponent } from './components/pages/charity/charity.component';
+import { TermsComponent } from './components/pages/terms/terms.component';
+import { AboutUsComponent } from './components/pages/about-us/about-us.component';
 
 export const appRoutes: Routes = [
   { path: '', pathMatch: 'full', component: LandingComponent },
@@ -20,5 +25,10 @@ export const appRoutes: Routes = [
     path: 'calendar',
     loadChildren: './components/calendar-page/calendar-page.module#CalendarPageModule' 
   },
+  { path: 'about-us', pathMatch: 'full', component: AboutUsComponent }, 
+  { path: 'terms', pathMatch: 'full', component: TermsComponent }, 
+  { path: 'charity', pathMatch: 'full', component: CharityComponent }, 
+  { path: 'contact', pathMatch: 'full', component: ContactComponent }, 
+  { path: 'service-provider', pathMatch: 'full', component: ServiceProviderComponent }, 
   { path: '**', pathMatch: 'full', component: PageNotFoundComponent }
 ];
